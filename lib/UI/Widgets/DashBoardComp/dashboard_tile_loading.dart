4f8872871a2cea8b03be4e58cf11f259d0216dd5
RafihYahya <rafihyahya@gmail.com>
2024-03-27 19:36:04 +0000

    ...

---
+++ lib/UI/Widgets/DashBoardComp/dashboard_tile_loading.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DashboardTileLoading extends StatelessWidget {
+  const DashboardTileLoading({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
